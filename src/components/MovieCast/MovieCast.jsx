e7cafebbb5fc5a8b513dcf504c95a2ffd214c092
TMelnychenko <tanyadance30@gmail.com>
2025-01-15 13:49:50 +0200

    some change

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -53,11 +53,15 @@ const MovieCast = () => {
         <div className={s.cast_list}>
           {casts.map((actor) => (
             <div key={actor.id} className={s.actor_card}>
-              <img
+               {casts.profile_path ? (
+                <img
                 src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`}
                 alt={actor.name}
                 className={s.actor_image}
-              />
+                />
+              ) : (
+                <p className={s.no_image}>No image available</p>
+              )}
               <div className={s.actor_info}>
                 <h4>{actor.name}</h4>
                 <p>{actor.character}</p>
